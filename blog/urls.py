24e90fa0246e6a7e148843138bee47506d900cdf
Aman <amansoni93744@gmail.com>
2020-06-03 04:02:51 +0530

    about contact added

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.postblog, name='postblog'),
+    path('about.html', views.about, name='about'),
+    path('contact.html', views.contact, name='contact'),
+    path('home.html', views.postblog, name='postblog'),
 ]
