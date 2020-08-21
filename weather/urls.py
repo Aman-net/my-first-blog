f82b56ca504ab5ef79a965b82540347c5286cfe2
Aman-net <amansoni93744@gmail.com>
2020-08-22 00:03:35 +0530

    2nd commmit

--- weather/urls.py
+++ weather/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.show_weather, name='weather'),
-    path('facebook', views.fb, name='fb'),
-    path('instagram', views.ig, name='ig'),
+    path('facebook/', views.fb, name='fb'),
+    path('instagram/', views.ig, name='ig'),
 ]
